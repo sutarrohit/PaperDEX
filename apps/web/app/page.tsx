495b83314a54d711fe7b44d5e562ee599c30f22a
sutarrohit <rohitsutar082@gmail.com>
2025-03-23 14:00:52 +0530

    feat: add join waitlist feature to the app
    - Implemented waitlist functionality
    - Added UI for users to join the waitlist
    - Integrated backend API for storing waitlist data

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,71 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
 import Image from "next/image";
+import { useState } from "react";
+import { joinWaitlistAction } from "@/actions/join";
+
+function isValidEmail(email: string) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 
 export default function Home() {
+    const [email, setEmail] = useState<null | string>(null);
+    const [loading, setLoading] = useState(false);
+
+    async function joinWaitlist(email: string | null) {
+        try {
+            if (!email) return;
+
+            if (!isValidEmail(email)) {
+                toast.error("Please enter valid Email", {
+                    style: {
+                        background: "#ff0000", // Red background for errors
+                        color: "#ffffff" // White text
+                    },
+                    position: "top-right"
+                });
+                return;
+            }
+            setLoading(true);
+            const response = await joinWaitlistAction(email);
+
+            if (response) {
+                setEmail(null);
+                setLoading(false);
+                toast.success(response.message || "Successfully joined the waitlist!", {
+                    position: "top-right",
+                    style: {
+                        background: "#00ff00",
+                        color: "#000000"
+                    }
+                });
+            }
+        } catch (error: any) {
+            toast.error(error?.message || "failed", {
+                style: {
+                    background: "#ff0000", // Red background for errors
+                    color: "#ffffff" // White text
+                },
+                position: "top-right"
+            });
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className='w-full min-h-screen flex flex-col items-center'>
-            <div className='h-[60px] flex items-center justify-between p-4 w-full'>
-                <p className='font-bold text-xl sm:text-2xl lg:text-3xl text-[#A1A1A1]'>DeTr</p>
-                <Button className='border px-4 sm:px-8'>Contact us</Button>
+            <div className='h-[80px] flex items-center justify-between w-full sm:px-6 px-4'>
+                <div className='flex flex-col items-center'>
+                    <Image src='/logo-white.png' alt='chart' className='w-[35px] h-[35px] object-cover' width={50} height={50} />
+                    <p className='text-center bottom-[-20px] font-bold  text-[14px] text-[#cecaca]'>PaperDEX</p>
+                </div>
+
+                <Button className='border'>Contact us</Button>
             </div>
 
             <div className='w-full mt-10 flex flex-col gap-3 items-center'>
@@ -17,6 +76,26 @@ export default function Home() {
                         Experience seamless crypto trading with our demo platform. Trade risk-free, explore real-time markets, and refine your
                         strategies with confidence.
                     </p>
+                </div>
+
+                <div className='flex justify-center gap-3 w-[90%]'>
+                    <Input
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        className='sm:w-[300px]'
+                        type='email'
+                        placeholder='you@example.com'
+                    />
+                    <Button
+                        disabled={loading}
+                        onClick={() => {
+                            joinWaitlist(email);
+                        }}
+                        className='cursor-pointer'
+                    >
+                        Join Waitlist
+                    </Button>
                 </div>
             </div>
 
