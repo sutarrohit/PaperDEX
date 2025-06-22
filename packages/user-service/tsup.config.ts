452662559407dca591f704ebd3dca9dbba2ef7bd
sutarrohit <rohitsutar082@gmail.com>
2025-06-22 16:58:02 +0530

    feat:add actions

--- packages/user-service/tsup.config.ts
+++ packages/user-service/tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["./index.ts"],
-    noExternal: ["@paperdex"],
-    splitting: false,
-    bundle: true,
-    outDir: "./dist",
-    clean: true,
-    env: { IS_SERVER_BUILD: "true" },
-    loader: { ".json": "copy" },
-    minify: true,
-    sourcemap: true
+  entry: ["./src/server.ts"],
+  noExternal: ["@paperdex/*"],
+  splitting: false,
+  bundle: true,
+  outDir: "./dist",
+  clean: true,
+  env: { IS_SERVER_BUILD: "true" },
+  loader: { ".json": "copy" },
+  minify: true,
+  sourcemap: true,
 });
